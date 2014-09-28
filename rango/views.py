52c221164e8213a94812b8e85f1de9daaea1345c
Hale Bopp <cometbopp@gmail.com>
2014-09-28 12:28:26 +0100

    added about page

--- rango/views.py
+++ rango/views.py
@@ -15,3 +15,8 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render_to_response('rango/index.html', context_dict, context)
+
+def about(request):
+    context = RequestContext(request)
+    context_dict = {'aboutmessage': "This is the about page"}
+    return render_to_response('rango/about/about.html', context_dict, context)
